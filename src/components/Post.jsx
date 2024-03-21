c15b3df89b5171835d8f9f8de8a001d32c5a7d3a
Chris <Christophermn619@gmail.com>
2024-03-20 21:09:19 -0400

    Used state to manage likes in Post Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import Comment from './Comment';
+import React, { useState } from 'react';
 
 function Post(){
+
+    const [likes, setLikes] = useState(0);
+
+    function handleLike(){
+        setLikes(likes + 1);
+      }
+
     return(
       <div>
         <h3>Test Post</h3>
         <p>This is a test post we'll edit later!</p>
+        <p>Likes: {likes}</p>
+
+        <button onClick={handleLike}>Like</button>
+
         <p>Likes: 30</p>
         <p>Comments:</p>
 
